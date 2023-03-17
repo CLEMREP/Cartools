42f8a313d00a4cc39def16bf72c3b2554dd47f44
MaxVzl <maxime.vozelle@gmail.com>
2023-03-17 15:44:41 +0100

    :sparkles: manage filter for fuel page

--- frontend/lib/src/Services/ColorManager.dart
+++ frontend/lib/src/Services/ColorManager.dart
@@ -8,6 +8,7 @@ class ColorManager
   static const Color fourthly = Color(0xFFFF685F);
   static const Color invisible = Color(0x00000000);
 
+  static const Color primary50 = Color(0x5500CC66);
   static const Color secondary50 = Color(0x1123282D);
   static const Color thirdly50 = Color(0x55FFFFFF);
 }
