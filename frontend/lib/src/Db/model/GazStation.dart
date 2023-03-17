9d984824a6e6169f4a6faf2e1f0a2ffb4f62a82c
CLEMREP <contact@clement-repel.fr>
2023-03-17 17:14:47 +0100

    :art: Add name of station in list

--- frontend/lib/src/Db/model/GazStation.dart
+++ frontend/lib/src/Db/model/GazStation.dart
@@ -5,6 +5,8 @@ class GazStation
   int stationId;
   double latitude;
   double longitude;
+  String brand;
+  String name;
   String postalCode;
   String city;
   String address;
@@ -15,6 +17,8 @@ class GazStation
     required this.stationId,
     required this.latitude,
     required this.longitude,
+    required this.brand,
+    required this.name,
     required this.postalCode,
     required this.city,
     required this.address,
@@ -26,6 +30,8 @@ class GazStation
       stationId: json['station_id'],
       latitude: double.parse(json['latitude'].toString()) / 100000,
       longitude: double.parse(json['longitude'].toString()) / 100000,
+      brand: json['brand'] ?? 'Inconnue',
+      name: json['name'] ?? 'Inconnue',
       postalCode: json['postal_code'],
       city: json['city'],
       address: json['address'],
